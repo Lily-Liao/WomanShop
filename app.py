2f2e65dd75e192e89490a0844a4ff00b8a051a7a
Lily-Liao <j5328n@gmail.com>
2020-12-24 00:16:07 +0800

    html+css practice

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route('/')
+@app.route('/product')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
+
+if __name__=='__main__':
+    app.run(debug=True)#,host='0.0.0.0',port=8080
